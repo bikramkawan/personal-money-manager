63341086524ddd21801a762957ecb757a9027ebe
bikramkawan <bikramkawan@gmail.com>
2017-08-15 01:47:17 +0200

    income category prototype added

--- src/components/Report.js
+++ src/components/Report.js
@@ -5,6 +5,7 @@ import React, {Component} from 'react';
 import BudgetSummary from './Report/BudgetSummary';
 import * as _ from 'lodash';
 import BarChart from './Report/BarChart'
+import Income from './Report/Cateogry/Income';
 
 export default class Report extends Component {
 
@@ -17,17 +18,17 @@ export default class Report extends Component {
 
         const totalCredit = _.sumBy(this.props.data, (d)=> d.credit > 0 ? d.credit : 0);
         const totalDebit = _.sumBy(this.props.data, (d)=> d.debit > 0 ? d.debit : 0);
-        console.log(totalCredit, totalDebit)
         return {totalCredit: totalCredit, totalDebit: totalDebit}
 
     }
 
     render() {
         const summary = this.getSummary();
-
+        const income = {salary:summary.totalDebit}
         return (
             <div><BudgetSummary summary={summary}/>
                 <BarChart summary={summary}/>
+                <Income income = {income}/>
             </div>
 
         )
---
+++ src/components/Report/Cateogry/Income.js
@@ -0,0 +1,55 @@
+/**
+ * Created by bikramkawan on 8/15/17.
+ */
+import React, {Component} from 'react';
+import {Grid, Row, Col} from 'react-bootstrap'
+
+export default class Income extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        console.log(this.props.income)
+        return (<Grid className="grid makeTable Income">
+            <Row className='header'>
+                <Col md={6}>Income</Col>
+                <Col md={2}>Budget</Col>
+                <Col md={2}>Actual</Col>
+                <Col md={2}>Difference</Col>
+            </Row>
+            <Row className='title'>
+                <Col md={6}>Salary</Col>
+                <Col md={2}>0.0</Col>
+                <Col md={2}>{this.props.income.salary}</Col>
+                <Col md={2}>{this.props.income.salary}</Col>
+            </Row>
+            <Row className='title'>
+                <Col md={6}>Freelance</Col>
+                <Col md={2}>0.0</Col>
+                <Col md={2}>0</Col>
+                <Col md={2}>0</Col>
+            </Row>
+            <Row className='title'>
+                <Col md={6}>Gifts Received</Col>
+                <Col md={2}>0.0</Col>
+                <Col md={2}>0.0</Col>
+                <Col md={2}>0.0</Col>
+            </Row>
+            <Row className='title'>
+                <Col md={6}>Income Other</Col>
+                <Col md={2}>0.0</Col>
+                <Col md={2}>0.0</Col>
+                <Col md={2}>0.0</Col>
+            </Row>
+            <Row className='total'>
+                <Col md={6}>Total Income</Col>
+                <Col md={2}>0.0</Col>
+                <Col md={2}>{this.props.income.salary}</Col>
+                <Col md={2}>{this.props.income.salary}</Col>
+            </Row>
+        </Grid>)
+    }
+
+}
