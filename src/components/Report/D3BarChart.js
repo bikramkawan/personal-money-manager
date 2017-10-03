3169efce1a20b2bae7a9db941368e4ab929cd987
bikramkawan <bikramkawan@gmail.com>
2017-10-04 00:31:44 +0200

    update positive chart

--- src/components/Report/D3BarChart.js
+++ src/components/Report/D3BarChart.js
@@ -15,13 +15,12 @@ class D3BarChart extends Component {
         const selector = this.props.selector;
         // d3.select('.' + selector).append('div').text('hfasfasfaelloe')
 
-
+        d3.selectAll('svg').remove();
         var svg = d3.select('.' + selector).append('svg').attr('width', width).attr('height', height)
         const margin = {top: 10, right: 20, bottom: 30, left: 50};
         width = +svg.attr("width") - margin.left - margin.right;
         height = +svg.attr("height") - margin.top - margin.bottom;
 
-        var tooltip = d3.select(".app").append("div").attr("class", "toolTip");
 
         var x = d3.scaleBand().rangeRound([0, width]).padding(0.1),
             y = d3.scaleLinear().rangeRound([height, 0]);
@@ -31,12 +30,8 @@ class D3BarChart extends Component {
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
         const data = this.props.data;
 
-        x.domain(data.map(function (d) {
-            return d.name;
-        }));
-        y.domain([0, d3.max(data, function (d) {
-            return d.value;
-        })]);
+        x.domain(data.map((d) =>d.name));
+        y.domain([0, d3.max(data, (d) => d.value)]);
 
         g.append("g")
             .attr("class", "axis axis--x")
@@ -45,9 +40,10 @@ class D3BarChart extends Component {
 
         g.append("g")
             .attr("class", "axis axis--y")
-            .call(d3.axisLeft(y).ticks(5).tickFormat(function (d) {
-                return parseInt(d / 1000) + "K";
-            }).tickSizeInner([-width]))
+            .call(d3.axisLeft(y)
+                .ticks(5)
+                .tickFormat((d)=> d)
+                .tickSizeInner([-width]))
             .append("text")
             .attr("transform", "rotate(-90)")
             .attr("y", 6)
@@ -59,27 +55,12 @@ class D3BarChart extends Component {
         g.selectAll(".bar")
             .data(data)
             .enter().append("rect").classed('bar--positive', true)
-            .attr("x", function (d) {
-                return x(d.name);
-            })
-            .attr("y", function (d) {
-                return y(d.value);
-            })
+            .attr("x", (d) => x(d.name))
+            .attr("y", (d)=>y(d.value))
             .attr("width", x.bandwidth())
-            .attr("height", function (d) {
-                return height - y(d.value);
-            })
-
-            .on("mousemove", function (d) {
-                tooltip
-                    .style("left", d3.event.pageX +10  + "px")
-                    .style("top", d3.event.pageY + 10 + "px")
-                    .style("display", "inline-block")
-                    .html((d.label) + "<br>" + (d.value));
-            })
-            .on("mouseout", function (d) {
-               tooltip.style("display", "none");
-            });
+            .attr("height", (d) => height - y(d.value))
+            .append('title')
+            .text((d) => d.value)
 
 
     }
